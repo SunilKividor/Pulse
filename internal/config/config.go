e1925d682f0661e772f9b8b5c6ba7da8f61c4fcc
Sunil <sunilkividor@gmail.com>
2025-02-03 03:10:41 +0530

    refactoring code and new folder structure

---
+++ internal/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/spf13/viper"
+)
+
+type AwsConfig struct {
+	AccessKey string `mapstructure:"access_key"`
+	SecretKey string `mapstructure:"secret_key"`
+	Region    string `mapstructure:"region"`
+}
+
+func LoadAwsConfig() (*AwsConfig, error) {
+	viper.SetConfigName("aws")
+	viper.AddConfigPath("../configs")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("failed to laod the config: %v", err)
+	}
+
+	var cfg AwsConfig
+	if err := viper.UnmarshalKey("aws", &cfg); err != nil {
+		return nil, fmt.Errorf("failed to unmarshall config: %v", err)
+	}
+
+	return &cfg, nil
+}
+
+func (awsConfig *AwsConfig) NewAwsSession() (aws.Config, error) {
+	return aws.Config{
+		Credentials: credentials.NewStaticCredentials(
+			awsConfig.AccessKey,
+			awsConfig.SecretKey,
+			"",
+		),
+		Region: &awsConfig.Region,
+	}, nil
+}
